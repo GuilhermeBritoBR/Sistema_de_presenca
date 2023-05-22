b7c5b2923d7562cc0a7e27f16fd949d02ac26582
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-05-22 14:24:57 -0300

    1.1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function login()   {
-    var cpf = prompt("Digite seu CPF:").value;
-    var senha = prompt("Digite sua senha:").value;
-    var dados ={
-        'cpf': ['888', '999', '1010', '1111', '1212'],
-        'senha': ['123', '123', '123', '123', '123'],
+    let cpf = prompt("Digite seu CPF:");
+    let senha = prompt("Digite sua senha:");
+    const dados ={
+        'cpf': ['888', '999', '1010', '1111', '1212',],
+        'senha': ['123', '123', '123', '123', '123',],
     }
-    if( (dados.cpf.includes(cpf)) )
-    if( (dados.senha.includes(senha))){
+    if( cpf === dados.cpf[0])
+    if( senha === dados.senha[0]){
         window.location.assign("user.html");
     }
     else{
@@ -15,4 +15,8 @@ function login()   {
         alert("Senha ou CPF incorreto!");
     }
 
+}
+function registrar{
+    var aluno = document.getElementById("aluno");
+    var falta = document.getElementById("falta");
 }
