d6460e90b191212e25499b9a58eb7aaa5267cd7f
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-05-22 21:36:50 -0300

    1.2

--- index.js
+++ index.js
@@ -20,4 +20,10 @@ function registrar()  {
     var falta = prompt("Digite quantas faltas ele teve hoje:");
     var alt_result = "o aluno " +  aluno + " teve " + falta + " faltas hoje ";
     document.getElementById("aluno_lista").innerHTML= alt_result;
+    var link = document.createElement("a");
+    var file = new Blob([alt_result], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = "presença.txt";
+    link.click();
+    URL.revokeObjectURL(link.href);
 }
